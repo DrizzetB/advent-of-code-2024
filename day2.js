77d07e03b4f83c92d8ceed9c38efd1abb24e1948
DrizzetB <dawid.bogacki@gmail.com>
2025-03-26 11:51:02 +0000

    Day2 part2

--- day2.js
+++ day2.js
@@ -5,32 +5,26 @@ let input = `7 6 4 2 1
 8 6 4 4 1
 1 3 6 7 9`
 
-
-
-console.log(input.length)
 let lines = input.split("\n");
 let sumOfSafe = 0;
 for (let i=0; i < lines.length; i++) {
     let singleLine = lines[i].split(' ');
     let isIncreasing;
     let isSafe;
-    if(singleLine[0] < singleLine[1]){
+    if(Number(singleLine[0]) < Number(singleLine[1])){
         isIncreasing = true; // increasing
     } else {
         isIncreasing = false; // decreasing
     }
-    console.log(singleLine,isIncreasing)
     for(let j=0; j < singleLine.length - 1; j++) {
-        if((isIncreasing && singleLine[j] < singleLine[j+1] && (singleLine[j+1] - singleLine[j]) <= 3) || (!isIncreasing && singleLine[j+1] < singleLine[j] && (singleLine[j] - singleLine[j+1]) <=3)){
+        if((isIncreasing && (Number(singleLine[j]) < Number(singleLine[j+1])) && ((Number(singleLine[j+1]) - Number(singleLine[j])) <= 3)) || (!isIncreasing && Number(singleLine[j+1]) < Number(singleLine[j]) && (Number(singleLine[j]) - Number(singleLine[j+1])) <= 3)){
             isSafe = true;
         } else {
-            console.log(`not safe at ${j}`);
             isSafe = false;
             break;
         }
     }
     if(isSafe) {
-        console.log("Safe")
         sumOfSafe++;
     }
 }
